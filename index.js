519f4f00b76d82bc9f84f8bfe4979ddf0cb395c8
Hasan-Rifat <developerhr000.com>
2022-09-20 15:52:00 +0600

    start

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpurgqo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("agency-services")
+      .collection("allservices");
+
+    // read all items
+    // http://localhost:5000/services
+
+    app.get("/services", async (req, res) => {
+      const result = await serviceCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+    // create
+    // http://localhost:5000/service
+    app.post("/service", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+
+    // update
+    // http://localhost:5000/service/
+    app.put("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await serviceCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // delete
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(filter);
+      res.send(result);
+    });
+  } finally {
+  }
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
