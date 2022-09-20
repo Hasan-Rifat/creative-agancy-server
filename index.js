b851074abc141f9e02e37b43a800e000bcdbd941
Hasan-Rifat <developerhr000.com>
2022-09-20 20:27:58 +0600

    testing now

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(json());
 
 // mongodb
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpurgqo.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
